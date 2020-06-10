c5149443556e835ea39cf5c479de0bf6a8c3c6bd
nunmuthue <nunmuthu@gmail.com>
2020-06-10 16:28:46 +0530

    updated solution for ex_1

--- NunmuthuMVC/Controllers/MoviesController.cs
+++ NunmuthuMVC/Controllers/MoviesController.cs
@@ -10,6 +10,22 @@ namespace NunmuthuMVC.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies
         public ActionResult Random()
         {
@@ -38,7 +54,7 @@ namespace NunmuthuMVC.Controllers
         }
 
         //movies
-        public ActionResult Index(int? pageIndex, string  sortBy)
+        public ActionResult Index2(int? pageIndex, string  sortBy)
         {
             if (!pageIndex.HasValue)
                 pageIndex = 1;
