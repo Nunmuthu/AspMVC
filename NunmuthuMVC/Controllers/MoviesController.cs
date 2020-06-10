863c333f165a8f5fd7746ac81943aae5dd62ad49
nunmuthue <nunmuthu@gmail.com>
2020-06-10 13:48:54 +0530

    Action Parameters

--- NunmuthuMVC/Controllers/MoviesController.cs
+++ NunmuthuMVC/Controllers/MoviesController.cs
@@ -16,6 +16,21 @@ namespace NunmuthuMVC.Controllers
             return View(movie);
         }
 
+        public ActionResult Edit(int movieId)
+        {
+            return Content("id=" + movieId);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string  sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 
 }
