e64ce0fc0f04d5f6dabdae7c2063612b3cac3917
nunmuthue <nunmuthu@gmail.com>
2020-06-10 13:03:05 +0530

    Added movies MVC

---
+++ NunmuthuMVC/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NunmuthuMVC.Models;
+
+namespace NunmuthuMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Aladdin" };
+            return View(movie);
+        }
+
+    }
+
+}
