5fa1033e033c35ed9c48e79d34c27bdf77bbac20
nunmuthue <nunmuthu@gmail.com>
2020-06-10 14:02:31 +0530

    Convetion based routing

--- NunmuthuMVC/Controllers/MoviesController.cs
+++ NunmuthuMVC/Controllers/MoviesController.cs
@@ -31,6 +31,11 @@ namespace NunmuthuMVC.Controllers
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+"/"+month);
+        }
     }
 
 }
